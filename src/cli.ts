dec3bbd8ebf3d96aa521f8de2070abf0750b41ca
Lucía Cufré Aman <luchicufre@hotmail.com>
2026-09-15 10:06:41 -0300

    refactor: remove unused cli file

--- src/cli.ts
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import { runAgent } from "./agent/agent";
-
-let history: any[] = [];
-
-const first = await runAgent("show me revenue by month", history);
-console.log(JSON.stringify(first.charts, null, 2));
